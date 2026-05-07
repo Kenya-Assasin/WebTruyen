768fcd723a3986840251863c87c868ac627accc4
Kenya-Assasin <shadowidmc156@gmail.com>
2026-05-07 07:32:05 +0700

    Fix 6

--- components/MangaList.tsx
+++ components/MangaList.tsx
@@ -106,7 +106,7 @@ export default function MangaList() {
 
                 <>
                     {/* GRID */}
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-5 gap-4">
                     {currentData.map((item) => (
                         <Link key={item.id} href={`/story/${item.id}`}>
                         <div
